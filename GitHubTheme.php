38d0efe50adf589ac308a26d2af9425df9e39ece
Fredrik Ekdahl <fekdahl@gmail.com>
2024-08-09 16:51:53 +0200

    Palette selection, not yet used

--- GitHubTheme.php
+++ GitHubTheme.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace GitHub;
 
+use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Menu;
+use Fisharebest\Webtrees\Session;
+use Fisharebest\Webtrees\Tree;
 use Fisharebest\Webtrees\Module\MinimalTheme;
 use Fisharebest\Webtrees\Module\ModuleCustomInterface;
 use Fisharebest\Webtrees\Module\ModuleCustomTrait;
@@ -20,6 +25,26 @@ class GitHubTheme extends MinimalTheme implements ModuleCustomInterface, ModuleG
     public function title(): string
     {
         return 'GitHub';
+    }
+
+    /**
+     * Generate a list of items for the user menu.
+     *
+     * @param Tree|null $tree
+     *
+     * @return array<Menu>
+     */
+    public function userMenu(Tree|null $tree): array
+    {
+        return array_filter([
+            $this->menuPendingChanges($tree),
+            $this->menuMyPages($tree),
+            $this->menuThemes(),
+            $this->menuPalette(),
+            $this->menuLanguages(),
+            $this->menuLogin(),
+            $this->menuLogout(),
+        ]);
     }
 
     /**
@@ -69,11 +94,96 @@ class GitHubTheme extends MinimalTheme implements ModuleCustomInterface, ModuleG
     {
         $stylesheets = parent::stylesheets();
 
-        // NOTE - a future version of webtrees will allow the modules to be stored in a private folder.
-        // Only files in the /public/ folder will be accessible via the webserver.
-        // Since modules cannot copy their files to the /public/ folder, they need to provide them via a callback.
         $stylesheets[] = $this->assetUrl('css/theme.css');
+        $stylesheets[] = $this->assetUrl('css/palettes/' . $this->palette() . '.css');
 
         return $stylesheets;
+    }
+
+    /**
+     * Create a menu of palette options
+     *
+     * @return Menu
+     */
+    protected function menuPalette(): Menu
+    {
+        /* I18N: A colour scheme */
+        $menu = new Menu(I18N::translate('Palette'), '#', 'menu-color');
+
+        $palette = $this->palette();
+
+        foreach ($this->palettes() as $palette_id => $palette_name) {
+            $url = route('module', ['module' => $this->name(), 'action' => 'Palette', 'palette' => $palette_id]);
+
+            $submenu = new Menu(
+                $palette_name,
+                '#',
+                'menu-github-' . $palette_id . ($palette === $palette_id ? ' active' : ''),
+                [
+                    'data-wt-post-url' => $url,
+                ]
+            );
+
+            $menu->addSubmenu($submenu);
+        }
+
+        return $menu;
+    }
+
+    /**
+     * Switch to a new palette
+     *
+     * @param ServerRequestInterface $request
+     *
+     * @return ResponseInterface
+     */
+    public function postPaletteAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $user    = Validator::attributes($request)->user();
+        $palette = Validator::queryParams($request)->isInArrayKeys($this->palettes())->string('palette');
+
+        $user->setPreference('github-palette', $palette);
+
+        Session::put('github-palette', $palette);
+
+        return response();
+    }
+
+     /**
+     * @return array<string>
+     */
+    private function palettes(): array
+    {
+        $palettes = [
+            'dark'                => I18N::translate('Dark default'),
+            'dark-dimmed'         => I18N::translate('Dark dimmed'),
+            'dark-high-contrast'  => I18N::translate('Dark high contrast'),
+            'light'               => I18N::translate('Light default'),
+            'light-high-contrast' => I18N::translate('Light high contrast'),
+        ];
+
+        return $palettes;
+    }
+
+    /**
+     * @return string
+     */
+    private function palette(): string
+    {
+        // If we are logged in, use our preference
+        $palette = Auth::user()->getPreference('github-palette');
+
+        // If not logged in or no preference, use one we selected earlier in the session.
+        if ($palette === '') {
+            $palette = Session::get('github-palette');
+            $palette = is_string($palette) ? $palette : '';
+        }
+
+        // We haven't selected one this session? Use the default
+        if ($palette === '') {
+            $palette = 'dark';
+        }
+
+        return $palette;
     }
 };
